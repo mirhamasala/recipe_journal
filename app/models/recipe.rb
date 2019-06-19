2a56f9a328858bba1b656929572ebd143cd31723
Mirha Masala <mirha.masala@me.com>
2019-06-19 21:30:56 +0800

    Populate database

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,5 @@
 class Recipe < ApplicationRecord
-  has_many :measures, dependent: destroy
+  has_many :measures, dependent: :destroy
   has_many :ingredients, through: :measures
   validates :name, presence: true, uniqueness: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning the fridge.."
+Ingredient.destroy_all
+
+puts "Doing groceries.."
+ingredients = %w{ almonds banana asparagus avocado cauliflower coconut nori spinach walnuts }
+
+ingredients.each do |ingredient|
+  Ingredient.create!(name: ingredient)
+end
+
+puts "Preping the veggies.."
+Recipe.create!(
+  name: "Banana Cauliflower Smoothie",
+  servings: 1,
+  time: 5,
+  category: "breakfast",
+  method: "Blend it",
+  photo_url: "https://images.unsplash.com/photo-1494315153767-9c231f2dfe79",
+  link: "https://www.eat-this.org/"
+)
