22d918e2aae85d96eb0a2ab68188c7954c0be71f
Mirha Masala <mirha.masala@me.com>
2019-06-19 19:44:38 +0800

    Create ingredient model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_19_114212) do
+ActiveRecord::Schema.define(version: 2019_06_19_114400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
