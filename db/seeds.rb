1110f661cb18180e173afe6fa8b2b043d7007adf
Mirha Masala <mirha.masala@me.com>
2019-06-20 15:49:16 +0800

    Add styling recipe page

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 puts "Cleaning the fridge.."
+Measure.destroy_all
+Recipe.destroy_all
 Ingredient.destroy_all
 
 puts "Doing groceries.."
@@ -17,11 +19,18 @@ end
 
 puts "Preping the veggies.."
 Recipe.create!(
-  name: "Banana Cauliflower Smoothie",
-  servings: 1,
-  time: 5,
-  category: "breakfast",
-  method: "Blend it",
-  photo_url: "https://images.unsplash.com/photo-1494315153767-9c231f2dfe79",
-  link: "https://www.eat-this.org/"
+  name: "Vegane Poke Bowl",
+  servings: 4,
+  time: 30,
+  category: "bowls",
+  method: "Backofen auf 200 °C Ober-/Unterhitze vorheizen.
+  Melone in 3 cm große Würfel schneiden. Mit Öl und Salz in einer feuerfesten Form vermischen und 30 Minuten im Ofen backen.
+  Essig unterrühren, gut vermengen und weitere 30 Minuten im Ofen garen. Ab und zu umrühren.
+  Melone aus dem Ofen nehmen, vollständig abkühlen lassen und fein geschnittenes Nori unterheben. Über Nacht im Kühlschrank ziehen lassen.
+  Sushireis wie hier beschrieben zubereiten.
+  Karotten mit dem Spiralschneider in Spiralen schneiden oder alternativ fein raspeln.
+  Avocado in feine Scheiben schneiden. Erbsen 5 Minuten dämpfen, anschließend halbieren.
+  Alles in einer Bowl anrichten und mit etwas Öl, Essig und Hawaiisalz abschmecken.",
+  photo_url: "https://www.eat-this.org/wp-content/uploads/2017/08/Good-Life-Poke-Bowl-5-800x1200.jpg",
+  link: "https://www.eat-this.org/vegane-poke-bowl/"
 )
