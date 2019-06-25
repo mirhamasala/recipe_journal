0229cd89187f672f33fe548110c96abd146b291f
Mirha Masala <mirha.masala@me.com>
2019-06-25 19:34:17 +0800

    Clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "recipes#index"
-  resources :recipes do
+  resources :recipes, except: [:index] do
     resources :measures, only: [:new, :create]
   end
   resources :measures, only: [:destroy]
