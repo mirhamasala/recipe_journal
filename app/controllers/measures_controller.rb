c4f65efbdb1afd73c9327d63834303baae77e26e
Mirha Masala <mirha.masala@me.com>
2019-06-28 14:00:00 +0800

    Add authorization for measure policy

--- app/controllers/measures_controller.rb
+++ app/controllers/measures_controller.rb
@@ -4,10 +4,12 @@ class MeasuresController < ApplicationController
   def new
     @ingredient = Ingredient.new
     @measure = Measure.new
+    authorize @measure
   end
 
   def create
     @measure = @recipe.measures.create(measure_params)
+    authorize @measure
     if @measure.save
       redirect_back(fallback_location: new_recipe_measure_path(@measure.recipe))
     else
@@ -17,6 +19,7 @@ class MeasuresController < ApplicationController
 
   def destroy
     @measure = Measure.find(params[:id])
+    authorize @measure
     @measure.delete
     redirect_back(fallback_location: new_recipe_measure_path(@measure.recipe))
   end
---
+++ app/policies/measure_policy.rb
@@ -0,0 +1,24 @@
+class MeasurePolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def create?
+    user_is_admin?
+  end
+
+  def update?
+    user_is_admin?
+  end
+
+  def destroy?
+    user_is_admin?
+  end
+
+  private
+  def user_is_admin?
+    user.admin?
+  end
+end
