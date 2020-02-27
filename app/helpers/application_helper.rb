4453c1a94484599be70de7a74bdd5dc6abf905e1
Mirha Masala <mirha.masala@me.com>
2020-02-27 17:19:50 +0800

    Add photo helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,14 @@ module ApplicationHelper
     disable_indented_code_blocks: true)
     return markdown.render(text).html_safe
   end
+
+  def photo_for(recipe)
+    if recipe.photo?
+      cl_image_tag(recipe.photo)
+    elsif recipe.link_to_photo?
+      image_tag(recipe.link_to_photo)
+    else
+      image_tag("#{recipe.name.parameterize.underscore}.jpg")
+    end
+  end
 end
